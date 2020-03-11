387aa29235cf094280672837f04711d8a5d3bbf0
bibi <baranowskabeata1@gmail.com>
2020-03-11 18:42:41 +0100

    Remove active class from film titles and articles

--- js/script.js
+++ js/script.js
@@ -4,15 +4,21 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event)
   /* remove class 'active' from all films links */
+
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  
+  for (let activeLink of activeLinks) {
+  activeLink.classList.remove('active');
+}
   /* add class 'active' to the clicked link */
   /*remove class 'active' from all films */
+
+  const activeArticles = document.querySelectorAll('.posts article.active');
+  
+  for (let activeArticle of activeArticles) {
+  activeArticle.classList.remove('active');
+  }
   /*get 'href' attribute from the clicked link*/
   /*find the correct article using the selector (value of 'href' attribute) */
   /*add class 'active' to thecorrect article */
-}
-
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
 }
